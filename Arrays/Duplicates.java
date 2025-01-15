ad1f8200a1dc85bd0defb162e5044872342c4ad9
raam <janakirampedireddi@gmail.com>
2025-01-15 22:10:14 +0530

    last

--- Arrays/Duplicates.java
+++ Arrays/Duplicates.java
@@ -1,28 +1,70 @@
-
-import java.util.Arrays;
-
+//<<<<<<< HEAD
+//
+//import java.util.Arrays;
+//
+//public class Duplicates {
+//    public static void main(String[] args) {
+//        int arr[]={2,1,3,2,2,3};
+//        Arrays.sort(arr);
+//        int count;
+//        for(int i=0;i<arr.length;i++){
+//            count=0;
+//            for(int j=0;j<arr.length;j++){
+//                if(arr[i]==arr[j] && i!=j)
+//                {
+//                    count++;
+//                }
+//            }
+//            if(count>=1)
+//            {
+//                System.out.println(arr[i]);
+//                i+=count;
+//            }
+//            if(count==0){
+//                System.out.println(arr[i]);
+//            }
+//        }
+//    }
+//    
+//=======
+package Arrays;
+import java.util.Scanner;
 public class Duplicates {
-    public static void main(String[] args) {
-        int arr[]={2,1,3,2,2,3};
-        Arrays.sort(arr);
-        int count;
-        for(int i=0;i<arr.length;i++){
-            count=0;
-            for(int j=0;j<arr.length;j++){
-                if(arr[i]==arr[j] && i!=j)
-                {
-                    count++;
-                }
-            }
-            if(count>=1)
-            {
-                System.out.println(arr[i]);
-                i+=count;
-            }
-            if(count==0){
-                System.out.println(arr[i]);
-            }
-        }
-    }
-    
+public static void main(String arg[]) {
+	Scanner s= new Scanner(System.in);
+	int arr[]= {2,2,5,6,7,5};
+	int arr1[]=new int[arr.length-1];
+	int dup[]=new int[arr.length-1];
+	int count=0;
+	int k=0;
+	
+	for(int x:arr) {
+		count=0;
+	for(int i=0;i<=arr.length-1;i++) {
+	    if(x==arr[i]) {	
+	    	count++;
+	    }
+	    	}
+	if(count==1) {
+	arr1[k]=x;
+	k++;
+	}
+	else {
+		int top=0;
+		for(int y:arr1) {
+			if(x==y) {
+				top++;
+			}
+		}
+		if(top==0) {
+			arr1[k]=x;
+			
+		}
+	}
+}
+for(int j=0;j<arr1.length;j++) {	
+	System.out.print(arr1[j]+" ");
+}
+}
+>>>>>>> cb0c25f (latest)
 }
---
+++ Assignment_1/Search_Elem.java
@@ -0,0 +1,26 @@
+package Assignment_1;
+import java.util.Scanner;
+public class Search_Elem {
+public static void main(String arg[]) {
+	Scanner s=new Scanner(System.in);
+	int[] arr= {2,4,1,5};
+	for(int x:arr) System.out.print(x+" ");
+	System.out.println();
+	System.out.println("Enter element to search from the array");
+	int z=s.nextInt();
+	int flag=0;
+	for(int i=0;i<arr.length;i++) {
+		
+		if(arr[i]==z) {
+			System.out.println("Element found at "+i+" index");
+			flag=1;
+			break;
+		}
+			
+		}
+	if(flag==0) {
+		System.out.println("element not found");
+	}
+s.close();	
+}
+}
